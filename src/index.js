e0ac255bafa6abed23ad1716debe8564c6cddd87
markchamberlain99 <45742521+markchamberlain99@users.noreply.github.com>
2021-12-07 23:14:22 -0500

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -11,25 +11,39 @@ const greeting = {
   color: "red"
 };
 
-var greetingText = "Good Morning";
+var greetingText;
 
-const myToday = new Date().getTime();
-const myMonth = new Date().getMonth();
-const myYear = new Date().getFullYear();
-const myDate = new Date().getDate();
-const myNoon = new Date(myYear, myMonth, myDate, 12).getTime();
-const myEvening = new Date(myYear, myMonth, myDate, 17).getTime();
+const myHour = new Date().getHours();
+// const myToday = new Date().getTime();
+// const myMonth = new Date().getMonth();
+// const myYear = new Date().getFullYear();
+// const myDate = new Date().getDate();
+// const myNoon = new Date(myYear, myMonth, myDate, 12).getTime();
+// const myEvening = new Date(myYear, myMonth, myDate, 17).getTime();
 
-if (myToday > myEvening) {
-  greetingText = "Good Evening";
-  greeting.color = "blue";
-} else if (myToday > myNoon) {
+//console.log(myHour);
+
+if (myHour < 12) {
+  greetingText = "Good Morning";
+} else if (myHour < 18) {
   greetingText = "Good Afternoon";
   greeting.color = "green";
+} else {
+  greetingText = "Good Evening";
+  greeting.color = "blue";
 }
+// if (myToday > myEvening) {
+//   greetingText = "Good Evening";
+//   greeting.color = "blue";
+// } else if (myToday > myNoon) {
+//   greetingText = "Good Afternoon";
+//   greeting.color = "green";
+// }
 //console.log(myMonth, myYear, myDate, myNoon, myToday);
 
 ReactDom.render(
-  <h1 style={greeting}>{greetingText}</h1>,
+  <h1 className="heading" style={greeting}>
+    {greetingText}
+  </h1>,
   document.getElementById("root")
 );
